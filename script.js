8d14a9c72abb4eedc9d6f241368384b2baf886b4
Lordmizzy29 <115583224+Lordmizzy29@users.noreply.github.com>
2022-11-03 00:07:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let selectedColors = ['blue', 'red', 'green', 'pink'];
+selectedColors[3] =2;
+
+var Videogames  = ['Gta','call ofduty','fortnite','roblox','Nba 2k'];
+console.log(Videogames[0]);
+console.log(Videogames[1]);
+console.log(Videogames[4]);
+
+Videogames.push('forza')
+Videogames.pop('fortnite')
+
+var numberOfVideogames = Videogames.length;
